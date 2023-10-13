15f4d8fbd52335567339a605a60d0519b6954515
DuskEVE <x987000@gmail.com>
2023-10-13 11:04:55 +0800

    more nest loop practise

--- practise_02.php
+++ practise_02.php
@@ -19,6 +19,12 @@
     .first{
         background-color: lightsalmon;
     }
+    .odd{
+        background-color: #ccc;
+    }
+    .hide{
+        border: 2px;
+    }
 
 </style>
 
@@ -30,7 +36,9 @@
     for($i=1; $i<10; $i++){
         array_push($arr, '<tr>');
         for($j=1; $j<10; $j++){
-            $str = '<td>'.$i.' * '.$j.' = '.($i * $j).'</td>';
+            $str = $i.' * '.$j.' = '.($i * $j).'</td>';
+            if($i % 2 != 0) $str = '<td class="odd">'.$str;
+            else $str = '<td>'.$str;
             // if( !($i == 9 && $j == 9)) $str = $str.',';
 
             // if(strlen($str) == 9) $str = $str."$nbsp$nbsp";
@@ -48,24 +56,63 @@
 
 <?php
 
-    $result = [];
+    $arr = [];
 
     for($i=0; $i<10; $i++){
-        array_push($result, '<tr>');
+        array_push($arr, '<tr>');
         for($j=0; $j<10; $j++){
             $str = '';
             
-            if($i == 0 && $j == 0) $str = '<td class="first">'.$str.'';
+            if($i == 0 && $j == 0) $str = '<td class="first">'.$str.'-';
             else if($i == 0) $str = '<td class="first">'.$str.$j;
             else if($j == 0) $str = '<td class="first">'.$str.$i;
-            else $str = '<td>'.$str.($i * $j);
+            else{
+                if($i % 2 != 0) $str = '<td class="odd">'.$str.($i * $j);
+                else $str = '<td>'.$str.($i * $j);
+            }
 
-            array_push($result, $str.'</td>');
+            array_push($arr, $str.'</td>');
         }
-        array_push($result, '</tr>');
+        array_push($arr, '</tr>');
     }
 
-    $result = join("", $result);
-    echo '<table>'.$result.'</table>';
+    $arr = join("", $arr);
+    echo '<table>'.$arr.'</table>';
+
+?>
+
+<br><h3>9*9 交叉表格(顯示一半)</h3><br>
+
+<?php
+
+    $arr = [];
+
+    for($i=0; $i<10; $i++){
+        array_push($arr, '<tr>');
+        for($j=0; $j<10; $j++){
+            $str = '';
+
+            // hide upright table
+            if($i < $j){
+                $str = '<td class="hide">';
+                array_push($arr, $str.'</td>');
+                continue;
+            }
+            
+            if($i == 0 && $j == 0) $str = '<td class="first">'.$str.'-';
+            else if($i == 0) $str = '<td class="first">'.$str.$j;
+            else if($j == 0) $str = '<td class="first">'.$str.$i;
+            else{
+                if($i % 2 != 0) $str = '<td class="odd">'.$str.($i * $j);
+                else $str = '<td>'.$str.($i * $j);
+            }
+
+            array_push($arr, $str.'</td>');
+        }
+        array_push($arr, '</tr>');
+    }
+
+    $arr = join("", $arr);
+    echo '<table>'.$arr.'</table>';
 
 ?>
